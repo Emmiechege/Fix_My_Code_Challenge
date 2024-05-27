e4a4173b2f5144d47554922a1483438986835710
Emmiechege <emilywanjachege@gmail.com>
2024-05-27 13:54:29 +0300

    fix the given code

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+square class module
+"""
+
+
+class Square():
+    """Describes the square"""
+
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """initializing the class"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """Perimeter of the square"""
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """string representation of the square"""
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    """create an object instance of a square class"""
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
